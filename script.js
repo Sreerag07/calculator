38b4b6fc693aaa60e69e4d700e767ef755bc6a44
Sreerag <sgpakkam@gmail.com>
2021-06-23 10:55:09 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,17 @@
+function display_value(num){
+    let inputbox=document.getElementById("inpt")
+    inputbox.value+=num
+}
+
+function evaluateExpression(){
+    let cur_value=inpt.value;
+    let res=eval(cur_value)
+    inpt.value=res
+}
+function clearBox(){
+    inpt.value="";
+}
+// function removeElement(){
+//     inpt.value=inpt.value.slice(0,-1)
+// }
+// this is for delete one value key
